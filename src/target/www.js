e29c7b7ec96bc44ce104c3f4b4352d07a011dc24
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-06-08 21:06:08 +0200

    extract json

--- src/target/www.js
+++ src/target/www.js
@@ -1,35 +1,12 @@
+const static = require("./www.static");
 // Create urls for https://www.artsdatabanken.no
 
 // NN-LA-M-S        =>  https://artsdatabanken.no/nin/LA/M/S
 // NN-LA-KLG-REIA-1 => https://artsdatabanken.no/nin/LA/KLG/REIA/1
 
-const fixed = {
-  NN: "https://artsdatabanken.no/Pages/179721",
-  "NN-LA": "https://artsdatabanken.no/nin/landskap",
-  "NN-LA-TI": "https://artsdatabanken.no/nin/landskap/typeinndeling",
-  "NN-NA": "https://artsdatabanken.no/Pages/222921",
-  "NN-LA-KLG": "https://artsdatabanken.no/nin/landskap/gradienter",
-  "NN-LA-KLG-JP": "https://artsdatabanken.no/Pages/262120",
-  "NN-LA-KLG-VP": "https://artsdatabanken.no/Pages/262126",
-  "NN-LA-KLG-BP": "https://artsdatabanken.no/Pages/262117",
-  "NN-LA-KLG-IP": "https://artsdatabanken.no/Pages/262119",
-  "NN-LA-KLG-VE": "https://artsdatabanken.no/Pages/262125",
-  "NN-LA-KLG-AIKS": "https://artsdatabanken.no/Pages/262160",
-  "NN-LA-KLG-AI": "https://artsdatabanken.no/Pages/262116",
-  "NN-LA-KLG-KA": "https://artsdatabanken.no/Pages/262121",
-  "NN-LA-KLG-IYK": "https://artsdatabanken.no/Pages/262118",
-  "NN-LA-KLG-REKS": "https://artsdatabanken.no/Pages/262124",
-  "NN-LA-KLG-REIDKF": "https://artsdatabanken.no/Pages/262122",
-  "NN-LA-KLG-REIA": "https://artsdatabanken.no/Pages/262123",
-  "NN-NA-TI": "https://artsdatabanken.no/Pages/180023",
-  "NN-NA": "https://artsdatabanken.no/Pages/222921",
-  "NN-NA-LKM": "https://artsdatabanken.no/Pages/179717",
-  "NN-NA-BS": "https://artsdatabanken.no/Pages/179723"
-};
-
 function makeUrl(query) {
   if (!query.kode) return null;
-  if (fixed[query.kode]) return fixed[query.kode];
+  if (static[query.kode]) return static[query.kode];
   console.warn(query);
   const parts = query.kode.replace("-TI", "").split("-");
   if (parts.length < 1) return null;
